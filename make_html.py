617ba3500819ba2957cb60e03b7a6a6612242aca
chvmvd <chvmvd@users.noreply.github.com>
2022-11-06 14:27:20 +0900

    make_html.pyで変数に保存してから書き出す設定にした

--- make_html.py
+++ make_html.py
@@ -6,29 +6,32 @@ with open("tmp/pronounces.csv") as f:
     inputs = [row for row in reader]
 
 
-f = open("tmp/main.html", "w")
-f.write('<head><link rel="stylesheet" href="./style.css" /></head>')
+html = ""
+html += '<head><link rel="stylesheet" href="./style.css" /></head>'
 for input in inputs:
     hanzi = input[0]
     files = sorted(glob.glob(f"tmp/{hanzi}*.svg"))
-    f.write('<div class="char">')
-    f.write('<div class = "title" lang = "zh-cmn-Hans" >')
-    f.write(f'<div class="pinyin">{input[1]}</div>')
-    f.write(f'<div class="hanzi">{hanzi}</div>')
-    f.write('</div>')
-    f.write('<div class = "wrapper">')
-    f.write('<div class = "strokes" >')
+    html += '<div class="char">'
+    html += '<div class = "title" lang = "zh-cmn-Hans" >'
+    html += f'<div class="pinyin">{input[1]}</div>'
+    html += f'<div class="hanzi">{hanzi}</div>'
+    html += '</div>'
+    html += '<div class = "wrapper">'
+    html += '<div class = "strokes" >'
     for file in files:
         svg = open(file, "r")
         data = svg.read()
-        f.write(str(data))
+        html += str(data)
         svg.close()
-    f.write('</div>')
-    f.write('<div class = "playgrounds" >')
-    f.write(
-        f'<div class = "playground" lang = "zh-cmn-Hans" style = "color: #cccccc" > {hanzi} </div>')
+    html += '</div>'
+    html += '<div class = "playgrounds" >'
+    html += f'<div class = "playground" lang = "zh-cmn-Hans" style = "color: #cccccc" > {hanzi} </div>'
     for i in range(5):
-        f.write('<div class = "playground" > </div>')
-    f.write('</div>')
-    f.write('</div>')
-    f.write('</div>')
+        html += '<div class = "playground" > </div>'
+    html += '</div>'
+    html += '</div>'
+    html += '</div>'
+
+f = open("tmp/main.html", "w")
+f.write(html)
+f.close()
