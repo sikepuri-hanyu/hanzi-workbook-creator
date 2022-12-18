72b9f5a2eece5ddf9b8759dc4d54a8706376fc93
WATAHIKI Yuto <104971044+chvmvd@users.noreply.github.com>
2022-12-19 07:25:26 +0900

    Update Home.tsx

--- practice-hanzi/src/pages/Home/Home.tsx
+++ practice-hanzi/src/pages/Home/Home.tsx
@@ -243,7 +243,7 @@ export default function Home() {
                   setNewData(initialData);
                 }}
               >
-                確定
+                追加
               </button>
             </td>
           </tr>
