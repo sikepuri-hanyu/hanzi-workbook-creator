465595c5a2ff172ce68d96a5315025c139fe9d4d
chvmvd <chvmvd@users.noreply.github.com>
2022-12-18 12:33:55 +0900

    SVG画像を拾ってきて表示することに成功

--- practice-hanzi/src/App.tsx
+++ practice-hanzi/src/App.tsx
@@ -1,17 +1,56 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import Hanzi from "./Hanzi";
+
+function getHanziCode(hanzi: string): number | undefined {
+  return hanzi[0].codePointAt(0);
+}
+
+async function getHanziData(hanzi: string) {
+  let result: string[] | undefined = [];
+  await fetch(
+    `https://raw.githubusercontent.com/parsimonhi/animCJK/master/svgsZhHans/${getHanziCode(
+      hanzi
+    )}.svg`
+  )
+    .then(async (response) => await response.text())
+    .then((text) => {
+      const reg = new RegExp(`id="z${getHanziCode(hanzi)}d.+" d=".*"`, "g");
+      result = text
+        .match(reg)
+        ?.map((char) => char.replace(/id=".*" d=/, "").replaceAll('"', ""));
+    });
+  return result;
+}
 
 function App() {
-  const [hanzi, setHanzi] = useState<string[]>(["一", "丁"]);
-  console.log(hanzi[0].codePointAt(0));
+  const [hoge, setHoge] = useState<string[] | undefined>(["b"]);
+  const [hanzis, setHanzis] = useState<string[]>(["一", "丁"]);
+  const [hanziDatas, setHanziDatas] = useState<string[][]>([[]]);
+  useEffect(() => {
+    (async () => {
+      const tmp = [];
+      for (const hanzi of hanzis) {
+        const tmp2 = await getHanziData(hanzi);
+        tmp.push(tmp2);
+      }
+      setHanziDatas(tmp);
+    })();
+  }, []);
   return (
     <>
-      <Hanzi
-        hanziNumber={hanzi[1].codePointAt(0)!}
-        width="100px"
-        height="100px"
-      ></Hanzi>
+      {hanziDatas.map((hanziData) => (
+        <svg
+          id="z19968"
+          className="acjk"
+          viewBox="0 0 1024 1024"
+          width="100px"
+          height="100px"
+        >
+          {hanziData.map((stroke) => (
+            <path id="z19968d1" d={stroke} />
+          ))}
+        </svg>
+      ))}
     </>
   );
 }
